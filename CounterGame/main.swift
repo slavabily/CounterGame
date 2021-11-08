c0edd50a27301b081316c4b0af466345a9b5a824
slava bily <slavabily@me.com>
2021-11-08 20:27:44 +0200

    flow for defining if 'n' is a power of '2' or next lower value is made

--- CounterGame/main.swift
+++ CounterGame/main.swift
@@ -8,9 +8,20 @@
 import Foundation
 
 func counterGame(n: Int) -> String {
-    
+    var i = 1
+    while i < n {
+        var t = pow(2, i)
+        if t == Decimal(n) {
+           print("\(t) bingo!")
+        } else if t > Decimal(n){
+            t = pow(2, i - 1)
+            print("\(t) is closest power of 2")
+            break
+        }
+        i += 1
+    }
     return "Richard"
 }
 
-print(counterGame(n: 6))
+print(counterGame(n: 128))
 
