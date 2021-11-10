1cbac616d71e26cd334cf9cd183e641e13a1701e
slava bily <slavabily@me.com>
2021-11-10 11:19:12 +0200

    successful run of code

--- CounterGame/main.swift
+++ CounterGame/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 func counterGame(n: Int) -> String {
     var v = Decimal(n)
-    var p = 1
+    var p = 0
     while v > 1 {
         var i = 1
         while Decimal(i) < v {
@@ -18,18 +18,17 @@ func counterGame(n: Int) -> String {
                 print("\(t) bingo!")
                 v = t / 2
                 print(v)
-                p = p + 1
                 break
             } else if t > v {
                 t = pow(2, i - 1)
                 print("\(t) is closest power of 2")
                 v = v - t
                 print(v)
-                p = p + 1
                 break
             }
             i += 1
         }
+        p = p + 1
     }
     if p % 2 == 0 {
         return "Richard"
